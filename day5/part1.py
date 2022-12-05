c406c10948b642a1c7c5f159c6cd261113a2c4b3
Josh Stein <s.joshua.stein@gmail.com>
2022-12-05 07:15:40 -0600

    cleaned up syntax

--- day5/part1.py
+++ day5/part1.py
@@ -1,5 +1,4 @@
 # D4P1
-from copy import deepcopy
 
 #fname = 'example.txt'
 fname = 'input1.txt'
@@ -18,22 +17,21 @@ stack9 = ['W', 'Z', 'H', 'G', 'C', 'J', 'M', 'B']
 
 data = fp.read().splitlines()[10:]  # Jump past the crate positional info. More cheese?
 
-state1 = {1: stack1, 2: stack2, 3: stack3, 4: stack4, 5: stack5, 6: stack6, 7: stack7, 8: stack8, 9: stack9}
-state2 = deepcopy(state1)
+state = {1: stack1, 2: stack2, 3: stack3, 4: stack4, 5: stack5, 6: stack6, 7: stack7, 8: stack8, 9: stack9}
 
 for line in data:
     qty = int(line.split(" ")[1])
     fstack = int(line.split(" ")[3])
     tstack = int(line.split(" ")[5])
-    rstack = state1[fstack][:qty]
+    rstack = state[fstack][:qty]
     rstack.reverse()
-    state1[tstack][:0] = rstack
-    del state1[fstack][:qty]
+    state[tstack][:0] = rstack
+    del state[fstack][:qty]
 
 endposition = ''
 i = 1
-for j in state1.items():
-    endposition += state1[i][0]
+for j in state.items():
+    endposition += state[i][0]
     i += 1
 
 fp.close()
--- day5/part2.py
+++ day5/part2.py
@@ -1,5 +1,4 @@
 # D4P2
-from copy import deepcopy
 
 #fname = 'example.txt'
 fname = 'input1.txt'
@@ -18,21 +17,20 @@ stack9 = ['W', 'Z', 'H', 'G', 'C', 'J', 'M', 'B']
 
 data = fp.read().splitlines()[10:]  # Jump past the crate positional info. More cheese?
 
-state1 = {1: stack1, 2: stack2, 3: stack3, 4: stack4, 5: stack5, 6: stack6, 7: stack7, 8: stack8, 9: stack9}
-state2 = deepcopy(state1)
+state = {1: stack1, 2: stack2, 3: stack3, 4: stack4, 5: stack5, 6: stack6, 7: stack7, 8: stack8, 9: stack9}
 
 for line in data:
     qty = int(line.split(" ")[1])
     fstack = int(line.split(" ")[3])
     tstack = int(line.split(" ")[5])
-    rstack = state2[fstack][:qty]
-    state2[tstack][:0] = rstack
-    del state2[fstack][:qty]
+    rstack = state[fstack][:qty]
+    state[tstack][:0] = rstack
+    del state[fstack][:qty]
 
 endposition = ''
 i = 1
-for j in state1.items():
-    endposition += state2[i][0]
+for j in state.items():
+    endposition += state[i][0]
     i += 1
 
 fp.close()
