0349048a385e0d93179a5d0e100c6605b9f8b10b
Josh Stein <s.joshua.stein@gmail.com>
2022-12-06 06:56:55 -0600

    day 6 commit

---
+++ day6/part2.py
@@ -0,0 +1,26 @@
+# D5P2
+
+#fname = 'example.txt'
+fname = 'input1.txt'
+
+
+def find_duplicate(substr):
+    # print(f'substr: {substr}')
+    for c in substr:
+        if substr.count(c) > 1:
+            # print(f'found dup char: {c}')
+            return substr.rfind(c)
+    return 0
+
+
+clen = 14   # number of chars to process
+cpos = 0   # char position
+
+fp = open(fname, 'r')
+data = fp.readlines()
+fp.close()
+
+while find_duplicate(data[0][cpos:cpos+clen]) > 0:
+    cpos += 1
+
+print(cpos+clen)
